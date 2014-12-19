56778b1b72fa484ae2959b374befe2116e6c6839
wicky <wicky@wicky-VirtualBox.(none)>
2014-12-19 17:25:26 +0800

    Added get_video_links function and download function

--- video_dl.py
+++ video_dl.py
@@ -28,10 +28,61 @@ class VideoDl(object):
             for page_link in page_links:
                 if page_link not in page_links_list:
                     page_links_list.append(page_link)
+        self.page_links_list = page_links_list
+
         return page_links_list
+
+    def get_video_links(self):
+        video_links_list = []
+        for page_link in self.page_links_list:
+            req = urllib2.Request(page_link, headers=hdr)
+            response = urllib2.urlopen(req)
+            html = response.read()
+            print html
+            pattern_vid = "\'file\'\,\'\S*\'\)"
+            pattern_seccode = "\'seccode\'\,\'\S*\'\)"
+            pattern_max_vid = "\'max_vid\'\,\'\S*\'\)"
+            vid = re.findall(pattern_vid, html)[0]
+            seccode = re.findall(pattern_seccode, html)[0]
+            max_vid = re.findall(pattern_max_vid, html)[0]
+            print vid, seccode, max_vid
+            get_file_link = self.domain + "/getfile.php?VID="+vid+\
+                    "&mp4=1&seccode="+seccode+"&max_vid="+max_vid
+            print get_file_link
+            ##req = urllib2.Request(get_file_link, headers=hdr)
+            ##response = urllib2.urlopen(req)
+            ##video_link = response.read()
+        return 
+         
+            
+
+    def download(self):
+        for url in self.video_links_list:
+            file_name = url.split('/')[-1]
+            u = urllib2.urlopen(url)
+            f = open(file_name, 'wb')
+            meta = u.info()
+            file_size = int(meta.getheaders("Content-Length")[0])
+            print "Downloading: %s Bytes: %s" % (file_name, file_size)
+            
+            file_size_dl = 0
+            block_sz = 8192
+            while True:
+                buffer = u.read(block_sz)
+                if not buffer:
+                    break
+            
+                file_size_dl += len(buffer)
+                f.write(buffer)
+                status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+                status = status + chr(8)*(len(status)+1)
+                print status,
+            
+            f.close()
 
     def main(self):
         self.get_page_links()
+        self.get_video_links()
 
 
 if __name__ == '__main__':
